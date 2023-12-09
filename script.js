218184712237e77faf257df50fc6c0bbeef44211
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2023-12-09 05:52:31 +0000

    Dec 9, 2023, 12:52 AM

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
-//show section J seat map image when button section J is clicked. hide the map in the background
 document.addEventListener("DOMContentLoaded", function() {
+    var currentStep = "section"; // Track the current step
+
     var sectionJButton = document.getElementById("section-J");
     var sectionJMap = document.getElementById("sectionJMap");
     var fullMap = document.getElementById("fullMap");
+    var selectionTitle = document.getElementById("selection-title");
     var selectionButtons = document.querySelector(".section-buttons");
     var rowButtons = document.querySelector(".row-buttons");
     var seatButtons = document.querySelector(".seat-buttons");
+    var backButton = document.getElementById("back-button"); // Back button
     var row5Button = document.getElementById("row-5");
     var row5Map = document.getElementById("row5Map");
-    var selectionTitle = document.getElementById("selection-title");
 
     sectionJButton.addEventListener("click", function() {
-        // Display the section J map and hide the full map
-        sectionJMap.style.display = "block";
-        fullMap.style.display = "none";
-        selectionButtons.style.display = "none";
-        rowButtons.style.display = "block";
-        selectionTitle.textContent = "Choose a row in Section J"
+        currentStep = "row"; // Update the current step
+        updateUI();
     });
 
     row5Button.addEventListener("click", function() {
-        seatButtons.style.display = "block";
-        rowButtons.style.display = "none";
-        row5Map.style.display = "block";
-        sectionJMap.style.display = "none";
-        selectionTitle.textContent = "Choose a seat in Section J Row 5"
+        currentStep = "seat"; // Update the current step
+        updateUI();
     });
 
+    backButton.addEventListener("click", function() {
+        // Logic to handle back button based on the current step
+        if (currentStep === "seat") {
+            currentStep = "row";
+        } else if (currentStep === "row") {
+            currentStep = "section";
+        }
+        updateUI();
+    });
+
+    function updateUI() {
+        // Hide all maps and buttons initially
+        sectionJMap.style.display = "none";
+        row5Map.style.display = "none";
+        fullMap.style.display = "none";
+        selectionButtons.style.display = "none";
+        rowButtons.style.display = "none";
+        seatButtons.style.display = "none";
+
+        // Show elements based on the current step
+        if (currentStep === "section") {
+            fullMap.style.display = "block";
+            selectionButtons.style.display = "block";
+            selectionTitle.textContent = "Choose your seating section";
+        } else if (currentStep === "row") {
+            sectionJMap.style.display = "block";
+            rowButtons.style.display = "block";
+            selectionTitle.textContent = "Choose a row in Section J";
+        } else if (currentStep === "seat") {
+            row5Map.style.display = "block";
+            seatButtons.style.display = "block";
+            selectionTitle.textContent = "Choose a seat in Section J Row 5";
+        }
+    }
+
+    // Initialize the UI
+    updateUI();
 });
